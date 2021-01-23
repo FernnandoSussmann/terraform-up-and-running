049468480a5a0c84f42da317804813be11ac9910
fernnandosussmann <fernnandosussmann@gmai.com>
2021-01-23 16:10:16 -0300

    Creating a autoscaling group

--- main.tf
+++ main.tf
@@ -33,13 +33,14 @@ provider "aws" {
     ssm            = "http://localhost:4583"
     stepfunctions  = "http://localhost:4585"
     sts            = "http://localhost:4592"
-  }
+    autoscaling    = "http://localhost:4593"
+ }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -47,9 +48,6 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  tags = {
-    Name = "terraform-example"
-  }
 }
 
 resource "aws_security_group" "instance" {
@@ -63,14 +61,35 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
 variable "server_port" {
     description = "The port the server will use to HTTP requests"
     type = number
     default = 8080
 }
 
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
 }
 
